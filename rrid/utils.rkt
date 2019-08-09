fd2cf3b842651d0394e8cd4f157a65ef1fd9cbcd
Tom Gillespie <tgbugs@gmail.com>
2019-08-08 20:42:18 -0700

    utils hr add #:fail keyword and test

--- rrid/utils.rkt
+++ rrid/utils.rkt
@@ -18,25 +18,28 @@
       [(_ predicate:id string-to-match:string ...+)
        #'(define (predicate value) (member value (list string-to-match ...)))]))
 
-(define (hr nested-hashes #:keys [keys-please #f] . keys)
+(define (hr nested-hashes #:fail [fail #f] #:keys [keys-please #f] . keys)
+  "#:fail -> raise a key error instead of ambiguously returning '() or #f"
   ; TODO allow multiple values for keys in addition to any-key
 
   (let* ([carkeys (if (null? keys) keys (car keys))]
          [end (cond
                [(null? keys) nested-hashes]
                [(and (syntax? carkeys) (eq? (syntax->datum carkeys) 'over-list))
-                (map (λ (list-hash) (apply hr list-hash (cdr keys) #:keys keys-please))
+                (map (λ (list-hash) (apply hr list-hash (cdr keys) #:keys keys-please #:fail fail))
                      nested-hashes)]
                #; ; works but makes it hard to infer the schema when reading
                [(and (syntax? carkeys) (eq? (syntax->datum carkeys) 'any-key) (list? nested-hashes))
                 (map (λ (list-hash) (apply hr list-hash (cdr keys) #:keys keys-please))
                      nested-hashes)]
                [(and (syntax? carkeys) (eq? (syntax->datum carkeys) 'any-key))  ; FIXME better errors on type mismatch
-                (map (λ (list-hash) (apply hr list-hash (cdr keys) #:keys keys-please))
+                (map (λ (list-hash) (apply hr list-hash (cdr keys) #:keys keys-please #:fail fail))
                      (hash-values nested-hashes))]
                [(null? (cdr keys))  ; FIXME skip on null ... aka splice it out?
-                (hash-ref nested-hashes carkeys null)]
-               [else (apply hr (hash-ref nested-hashes carkeys) (cdr keys) #:keys keys-please)]
+                (if fail
+                    (hash-ref nested-hashes carkeys)
+                    (hash-ref nested-hashes carkeys null))]
+               [else (apply hr (hash-ref nested-hashes carkeys) (cdr keys) #:keys keys-please #:fail fail)]
                )])
     (if keys-please
         (cond [(list? end) (remove-duplicates end)]
@@ -49,6 +52,8 @@
   (define res #hasheq((_source . #hasheq((synonyms . #hasheq((a . 1)
                                                              (b . 2)
                                                              (c . 3)))))))
+  (check-exn exn:fail? (λ () ((hr res '_source 'missing #:fail #t))))
+  (check-equal? '() (hr res '_source 'missing))
   (check-equal? (hr res '_source #:keys #t) '(synonyms))
   (hr res '_source 'synonyms #:keys #f))
 
